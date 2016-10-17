a0f3684d9d35d4f05e3e716f8eab54d7c59760b2
jonathandavidblack <jonnybl915@gmail.com>
2016-10-17 15:41:03 -0400

    added delete and check functionality

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,38 @@
 import { Template } from 'meteor/templating';
-
+import './task.js';
 import { Tasks } from '../api/tasks.js';
 
 import './body.html';
-
+import './task.js';
 Template.body.helpers({
   tasks() {
     return Tasks.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
+
+Template.body.helpers({
+  tasks() {
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
